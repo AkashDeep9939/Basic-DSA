9c5a26d431cbabdc0e7de20fd3bd7480c4d5fb17
Akash Deep <akashdeep9939@gmail.com>
2023-12-29 13:58:48 +0530

    Paradigm of programming

---
+++ c++2.cpp
@@ -0,0 +1,75 @@
+/*
+                    Programming Paradigm 
+    it means stype of programming
+
+    types of paradigm 
+    1) Monolithic Programming
+    2) Modular pragramming or procedural pragramming
+    3) Object oriented programming
+*/
+
+// Modular Programming
+/*
+ Here all the pragraam written inside a main function 
+ ex)
+
+ int main(){
+
+
+
+        // all code
+
+
+
+
+
+
+ }
+*/
+
+//Procedural or Modular programming
+
+/*
+  Here all pragram is written in funtion form and function perform its specific task 
+
+  ex)
+
+fun1(){
+    // perform its task like add number
+}
+
+fun2(){
+
+    // perform its task like sub number
+
+}
+
+*/
+
+// OOps programming
+
+/*
+
+int modular programming data data are seperated but in OOps data are bindede
+
+    ex)
+    class info{
+        data1;
+        data2;
+        data3;
+
+        function1(){
+
+        }
+
+        function2(){
+
+        }
+    };
+
+    int main(){
+        Info i;
+        i.function1();
+        i.function2();
+    }
+*/
