96531d94d7616ff9c58e4dd4b28cbdb7b1a987a9
Akash Deep <akashdeep9939@gmail.com>
2023-12-30 14:11:33 +0530

    frist program

---
+++ c++3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+    cout<<"Enter two number";
+    cin>>a>>b;
+
+    c = a+b;
+    cout<<"Addition is"<<c<<endl;
+
+    return 0;
+    
+}
