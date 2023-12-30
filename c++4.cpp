b8bd9e7830e0641bfcb252d1aa5f30cbe28e7c01
Akash Deep <akashdeep9939@gmail.com>
2023-12-30 14:17:17 +0530

    getting name withot sapce

---
+++ c++4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout<<"May I know your Name";
+    getline(cin,name);
+
+    cout<<"Hellow Mr"<<name;
+
+    return 0;
+    
+}
