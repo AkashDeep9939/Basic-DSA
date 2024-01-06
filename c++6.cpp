8f15ad3d2de5bedbe70962f4a7e7fc7b7e7c8826
Akash Deep <akashdeep9939@gmail.com>
2024-01-06 10:04:13 +0530

    triange basic code

---
+++ c++6.cpp
@@ -0,0 +1,13 @@
+// basic area of triangle code 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int b, h;
+    cout<<"Enter base and height";
+    cin>>b>>h;
+
+    int a = (b*h)/2;
+
+    cout<<"Area of triangle is "<<a;
+}
