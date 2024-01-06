e7106ba310262561ce4d0975165b0160e513c5c8
Akash Deep <akashdeep9939@gmail.com>
2024-01-06 09:55:08 +0530

    basic data type and variable in c++

---
+++ c++5.cpp
@@ -0,0 +1,71 @@
+// data types in c++
+// two type of data type numberic and character
+// numberic example are int 1 folat 2.5 
+
+// char data type are char a 
+
+// Data type in variable 
+
+// 1) primitive
+//Integram-> char int, bool  foloating-> float, duble;
+
+//2)User types
+//enum struct union class 
+
+// Derived => array pointer refrence;
+
+// char = 2 power 7 marns 128 bits => -128 to 127 
+
+/*
+A-65 ..............Z-90 
+a-97...............z-122
+0-48..............9-57
+*/
+
+// Modifiers 
+
+// unsigned int -> 0 - 65535
+// unsigned char -> 0 - 255
+// long in = 4 byte
+//           8 byte 
+
+// long double = 10 byte 
+
+// Variable 
+/*
+    variables are name given to data variable have data types variablle is place where we store data in program 
+*/
+
+// ex 
+
+int main(){
+    int rollNo; // this is declertion of variable 
+
+    rollNo = 10; // initilized variable
+}
+
+// Q) how to declate a variable 
+
+// int x1 // allowed 
+// int 1x // not allowed
+// int roll number // not allowed 
+// int roll_number; // allowed
+// int rollnumber // allowed
+// int Rollnumber // allowed
+
+// Arithmetic operator and operation 
+// operator is predefined in c++ they are useful for programming they have some mathematcs symbol like 
+// arithmetic operator -, +, *, /, %
+
+// mod operator only alloed in int or char data type
+
+// operator precidance in c++
+// 1) ()
+// 2) *, /, %,
+// +, -
+
+
+
+
+
+
